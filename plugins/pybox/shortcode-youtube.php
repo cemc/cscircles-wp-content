6b9dd9feeb90ea4c50f562abd8edefe617bd17a4
David Pritchard <daveagp@gmail.com>
2012-05-10 10:50:18 -0400

    removed 'related links' from embedded youtube

--- plugins/pybox/shortcode-youtube.php
+++ plugins/pybox/shortcode-youtube.php
@@ -5,7 +5,8 @@ add_shortcode('youtube', 'youtube');
 function youtube($options, $content) {
   $id = $options[0];
   return "<div style='text-align: center;'>
-<iframe width='560' height='315' src='http://www.youtube.com/embed/" . $id . "' frameborder='0' allowfullscreen>
+<iframe width='560' height='315' src='http://www.youtube.com/embed/" . $id . 
+    "?rel=0' frameborder='0' allowfullscreen>
 </iframe>
 </div>";
 }
