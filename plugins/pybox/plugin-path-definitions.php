e99cdb67c47d4b81f9fcc27d1be8fd87025e89f7
David Pritchard <daveagp@gmail.com>
2012-06-10 14:13:40 -0400

    js/css++

--- plugins/pybox/plugin-path-definitions.php
+++ plugins/pybox/plugin-path-definitions.php
@@ -57,8 +57,8 @@ define('UWARN', UFILES . 'warning.png');
 
 define('UPYBOX', UWPHOME . 'wp-content/plugins/pybox/');
 
-define('UPYBOXJS', UPYBOX . 'pybox.js?10');
-define('UPYBOXCSS', UPYBOX . 'pybox.css?4');
+define('UPYBOXJS', UPYBOX . 'pybox.js?11');
+define('UPYBOXCSS', UPYBOX . 'pybox.css?5');
 define('UDEBUGPHP', UPYBOX . 'pages/problem-options.php');
 define('UOLDHISTORY', UPYBOX . 'pages/problem-history.php?p=');
 
