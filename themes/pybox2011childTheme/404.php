a3700c6f720231747a32dfe077f527f9a33c42a9
David Pritchard <daveagp@gmail.com>
2012-08-30 22:32:38 -0400

    fix 404

--- themes/pybox2011childTheme/404.php
+++ themes/pybox2011childTheme/404.php
@@ -22,7 +22,7 @@ get_header(); ?>
 				<div class="entry-content">
 					<p>
 <?php      echo sprintf(__t("We cannot find anything at the address %s"), $niceurl)." ".
-	   sprintf(__t('Try a different address, a <a %1$s>search</a>, or <a %2$s>contact us</a> if the website sent you here via a broken link.'), 'href="'.siteurl('search/').'"', 'href="'.siteurl('contact/').'"');
+	   sprintf(__t('Try a different address, a <a %1$s>search</a>, or <a %2$s>contact us</a> if the website sent you here via a broken link.'), 'href="'.cscurl('search').'"', 'href="'.cscurl('contact').'"');
 ?></p>
 
 <form id="searchform" action="<?php echo cscurl('search'); ?>">
