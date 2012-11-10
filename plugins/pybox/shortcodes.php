48d065fb3328052719a492dfed977f8433431322
David Pritchard <daveagp@gmail.com>
2012-11-10 00:59:10 -0500

    fix locale/lang check

--- plugins/pybox/shortcodes.php
+++ plugins/pybox/shortcodes.php
@@ -287,7 +287,8 @@ function pyRecallHandler($options, $content) {
     return "[pyRecall error: no slug given]";
   
   global $wpdb;
-  $problem = $wpdb->get_row($wpdb->prepare("SELECT * FROM wp_pb_problems WHERE slug = %s", $options['slug']), ARRAY_A);
+  $problem = $wpdb->get_row($wpdb->prepare("SELECT * FROM wp_pb_problems WHERE slug = %s AND lang = %s",
+					   $options['slug'], pll_current_language()), ARRAY_A);
 
   if ($problem == NULL) 
     return "[pyRecall error: slug " . $options['slug'] . " not found]";
