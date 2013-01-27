077fe12f5b8251b95a4a4ebe17e69a5dc143ce93
David Pritchard <daveagp@gmail.com>
2013-01-27 18:31:34 -0500

    removed java parser to its own git

--- plugins/pybox/plugin-main.php
+++ plugins/pybox/plugin-main.php
@@ -172,8 +172,10 @@ require_once("newuseremail.php");
 #require_once("dbf-subs.php"); not in use
 #require_once("dbf-completed.php"); not in use
 require_once("css-admin.php");
-require_once("java.php");
 
+add_shortcode('javaTest', function($o, $c) {
+		passthru("/home/cscircles/java-syntax/tests.py");
+	      });
 
 add_action( 'wp_enqueue_scripts', 'add_button_stylesheet' );
 function add_button_stylesheet() {
