1901f7a6d68b5b42e5d3880e133566badccadc1f
David Pritchard <daveagp@gmail.com>
2013-06-13 19:20:10 -0400

    upgrading to v3 of visualizer, embedding

--- plugins/pybox/shortcodes.php
+++ plugins/pybox/shortcodes.php
@@ -11,6 +11,29 @@ add_shortcode('pyMultiScramble', 'pyMultiScrambleHandler');
 add_shortcode('newuserwelcome', 'newuserwelcome');
 add_shortcode('pyRecall', 'pyRecallHandler');
 
+add_shortcode('pyVis', 'pyVisHandler');
+
+function pyVisHandler($options, $content) {
+  $res = ' ';
+  $content = html_entity_decode(str_replace("\xC2\xA0", ' ', str_replace('<br/>', "\n", $content)));
+  $content = trim($content);
+  if (substr($content, -3)=='<p>') {
+    $content = substr($content, 0, strlen($content)-3);
+  }
+  $content = trim($content);
+  //  for ($i =0; $i<strlen($content); $i++)
+  //  $res .= ' ' . ord($content[$i]);
+  //return $res;
+  if (strlen($content) < 11 || substr($content, 0, 5) != '<pre>' || substr($content, -6) != "</pre>")
+    return "Error: interior of pyVis must be <pre><pre>...</pre></pre>Found <pre>".substr($content, 0, 5).' '.substr($content, strlen($content)-6, 6)."</pre>";
+  $content = trim(substr($content, 5, strlen($content)-11));
+  return
+    //    '<pre>'.$content.'</pre>'.
+    '<iframe width="100%" height="480" frameborder="0" src="../static/OnlinePythonTutor/v3/iframe-embed.html#'
+    .'code='.urlencode($content)
+    .'&cumulative=false&heapPrimitives=false&drawParentPointers=false&textReferences=false&showOnlyOutputs=false&py=3&curInstr=0"> </iframe>';
+}
+
 function loadMostRecent($slug) {
   if ( !is_user_logged_in() ) 
     return NULL;
