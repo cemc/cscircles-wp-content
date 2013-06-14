dd6f072a5adc34f7eca12b447274c5c4df4025cd
David Pritchard <daveagp@gmail.com>
2013-06-14 10:42:42 -0400

    new shortcode file

---
+++ plugins/pybox/shortcode-vis.php
@@ -0,0 +1,27 @@
+<?php
+
+add_shortcode('pyVis', 'pyVisHandler');
+
+function pyVisHandler($options, $content) {
+  global $pyRenderCount;
+  $id = $pyRenderCount++;
+  $res = ' ';
+  $content = html_entity_decode(str_replace("\xC2\xA0", ' ', str_replace('<br/>', "\n", $content)));
+  $content = trim($content);
+  if (substr($content, -3)=='<p>') {
+    $content = substr($content, 0, strlen($content)-3);
+  }
+  $content = trim($content);
+  //  for ($i =0; $i<strlen($content); $i++)
+  //  $res .= ' ' . ord($content[$i]);
+  //return $res;
+  if (strlen($content) < 11 || substr($content, 0, 5) != '<pre>' || substr($content, -6) != "</pre>")
+    return "Error: interior of pyVis must be <pre><pre>...</pre></pre>Found <pre>".substr($content, 0, 5).' '.substr($content, strlen($content)-6, 6)."</pre>";
+  $content = trim(substr($content, 5, strlen($content)-11));
+  return
+    //    '<pre>'.$content.'</pre>'.
+    '<iframe id="the_iframe'.$id.'" width="100%" height="480" frameborder="0" src="../static/OnlinePythonTutor/v3/iframe-embed.html#'
+    .'code='.urlencode($content)
+    .'&cumulative=false&heapPrimitives=false&drawParentPointers=false&textReferences=false&showOnlyOutputs=false&py=3&curInstr=0&containerID=the_iframe'.$id.'"> </iframe>';
+}
+
