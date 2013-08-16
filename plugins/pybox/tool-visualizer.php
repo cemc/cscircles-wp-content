586bb343bce3ae55693c34470ff40659603e87b6
David Pritchard <daveagp@gmail.com>
2013-08-16 15:53:09 -0400

    working on stub

--- plugins/pybox/tool-visualizer.php
+++ plugins/pybox/tool-visualizer.php
@@ -5,9 +5,8 @@ add_action('template_redirect', 'visualizer_tr');
 function visualizer_tr() {
   $url = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
   $homeurl = trim(parse_url(home_url(), PHP_URL_PATH), '/');
-  echo $url;
-  echo $homeurl;
-  if (! preg_match("@^".preg_quote($homeurl)."/visualizer(/|$)@", $url, $matchdummy))
+  if ($homeurl != '') $homeurl .= '/';
+  if (! preg_match("@^".preg_quote($homeurl)."visualizer(/|$)@", $url, $matchdummy))
     return;
 
   $wp_query->is_404 = false;
