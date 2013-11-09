315d2a235330b5a4ffd84ba8b6d8a723525bfa83
David Pritchard <daveagp@gmail.com>
2013-11-09 16:19:04 -0500

    trying to fix translations?

--- languages/get-it.sh
+++ languages/get-it.sh
@@ -5,5 +5,6 @@ wget -A mo,po -r -l 2 -nH --cut-dirs=5 http://svn.automattic.com/wordpress-i18n/
 wget -A mo,po -r -l 2 -nH --cut-dirs=4 http://svn.automattic.com/wordpress-i18n/de_DE/trunk/messages/
 cd twentyeleven
 wget -A mo,po -r -l 2 -nH --cut-dirs=5 http://svn.automattic.com/wordpress-i18n/fr_FR/branches/3.6/messages/twentyeleven
+rm robots.txt
 cd ..
 rm robots.txt
