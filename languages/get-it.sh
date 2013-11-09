8dc11c77587743996f29404decc9258212a36da0
David Pritchard <daveagp@gmail.com>
2013-11-09 15:53:04 -0500

    update translations

--- languages/get-it.sh
+++ languages/get-it.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 rm -rf *.{mo,po}
 rm -rf */*.{mo,po}
-wget -A mo,po -r -l 2 -nH --cut-dirs=5 http://svn.automattic.com/wordpress-i18n/fr_FR/branches/3.5/messages/
+wget -A mo,po -r -l 2 -nH --cut-dirs=5 http://svn.automattic.com/wordpress-i18n/fr_FR/branches/3.7/messages/
 wget -A mo,po -r -l 2 -nH --cut-dirs=4 http://svn.automattic.com/wordpress-i18n/de_DE/trunk/messages/
+cd twentyeleven
+wget -A mo,po -r -l 2 -nH --cut-dirs=5 http://svn.automattic.com/wordpress-i18n/fr_FR/branches/3.6/messages/twentyeleven
+cd ..
 rm robots.txt
