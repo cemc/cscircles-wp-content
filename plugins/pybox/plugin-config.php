baa7c60d8850201087f55af033a351e99a8a1773
David Pritchard <daveagp@gmail.com>
2014-12-12 00:55:01 -0500

    Lithuanian is live

--- plugins/pybox/plugin-config.php
+++ plugins/pybox/plugin-config.php
@@ -2,8 +2,8 @@
 
   // this file contains things you might need to personalize
 
-define('PRODUCTION_LANGUAGES', serialize(array('en', 'fr', 'de', 'nl')));
-define('DEVELOPMENT_LANGUAGES', serialize(array('lt')));
+define('PRODUCTION_LANGUAGES', serialize(array('en', 'fr', 'de', 'nl', 'lt')));
+define('DEVELOPMENT_LANGUAGES', serialize(array()));
 
 // user id of cscircles assistant for each language, defaults to dave/troy/sandy (0)
 // 2 should change
