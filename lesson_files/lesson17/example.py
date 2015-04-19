4556d1bb8d71173537a602f4000aec1e2bbe0093
David Pritchard <daveagp@gmail.com>
2015-04-19 12:49:50 -0400

    incorporate kindly stranger's rant about object/variable semantics

--- lesson_files/lesson17/example.py
+++ lesson_files/lesson17/example.py
@@ -1,6 +1,6 @@
 oldSize = ["letter", 8.5, 11]   # size of paper in inches
-newSize = oldSize               # make a copy
+newSize = oldSize               # make a copy?
 newSize[1] = newSize[1]*2.54    # convert to cm
 newSize[2] = newSize[2]*2.54    # convert to cm
 print(newSize)                  # as expected, prints ["letter", 21.59, 27.94]
-print(oldSize)                  # surprise! DOES NOT print ["letter", 8.5, 11]
+print(oldSize)                  # no longer holds ["letter", 8.5, 11]...
