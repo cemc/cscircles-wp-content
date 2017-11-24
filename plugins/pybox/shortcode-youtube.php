68383fe474fcbf9e432acbb8cdb9cb0686156f5c
David Pritchard <daveagp@gmail.com>
2017-11-24 11:45:45 -0500

    youtube https

--- plugins/pybox/shortcode-youtube.php
+++ plugins/pybox/shortcode-youtube.php
@@ -5,7 +5,7 @@ add_shortcode('youtube', 'youtube');
 function youtube($options, $content) {
   $id = $options[0];
   return "<div style='text-align: center;'>
-<iframe width='560' height='315' src='http://www.youtube.com/embed/" . $id . 
+<iframe width='560' height='315' src='https://www.youtube.com/embed/" . $id . 
     "?rel=0' frameborder='0' allowfullscreen>
 </iframe>
 </div>";
