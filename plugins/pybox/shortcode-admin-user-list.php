2970291c63bfa996aeebddfedeaf59ade5e832d3
David Pritchard <daveagp@gmail.com>
2017-11-30 01:19:29 -0500

    Dirty paging since user list size overflows apache

--- plugins/pybox/shortcode-admin-user-list.php
+++ plugins/pybox/shortcode-admin-user-list.php
@@ -7,9 +7,15 @@ function adminuserlist($options, $content) {
 
   //resendEmails();
 
+  // 10000 per page
+  $page = intval($_GET['chunk']);
+  $lo = $page*10000;
+  $hi = ($page+1)*10000;
+
   global $wpdb;
-  $rows = $wpdb->get_results('select id, user_login, user_email from '.$wpdb->prefix.'users');
-  $r = '<table><tr><th>id</th><th>login</th><th>email</th></tr>';
+  $rows = $wpdb->get_results('select id, user_login, user_email from '.$wpdb->prefix."users where id >= $lo and id < $hi ");
+  $r = '<a class="open-same-window" href="?chunk=' . ($page-1) . '">prev 10k</a> <a class="open-same-window" target="_self" href="?chunk=' . ($page+1) . '">next 10k</a>'; 
+  $r .= '<table><tr><th>id</th><th>login</th><th>email</th></tr>';
   foreach ($rows as $row) {
     $r .= sprintf("<tr><td>%s</td><td>%s</td><td>%s</td></tr>", 
 		  $row->id, $row->user_login, $row->user_email);
