073f8a537647f82c05bdb84ebbc208d458e93849
David Pritchard <daveagp@gmail.com>
2019-11-23 22:21:57 -0500

    Minor updates for migration

--- themes/pybox2011childTheme/header.php
+++ themes/pybox2011childTheme/header.php
@@ -103,7 +103,7 @@ if (isset($post)) {
   <a href="http://www.uwaterloo.ca"><img src="<?php echo UFILES . __t('wordmark-uw.gif');?>" style="margin: 0px;"/></a>
 <a href="http://cemc.uwaterloo.ca"><img src="<?php echo UFILES . __t('wordmark-cemc.png');?>" style="margin: 0px; position: absolute; right: 0px;"></a>
 	  </div>
-<div style='background-color: red'><center><b>This site is undergoing a migration &mdash; you are on an inactive server that will not save your work long-term. Please visit <tt><a href="https://cscircles.cemc.uwaterloo.ca">cscircles.cemc.uwaterloo.ca</a></tt> instead. Email any questions to <tt><a href="mailto:daveagp@gmail.com">daveagp@gmail.com</a></tt>.</b></center></div>
+<div style='background-color: yellow'><center><b>This site was recently migrated. Email any questions to <tt><a href="mailto:daveagp@gmail.com">daveagp@gmail.com</a></tt>.</b></center></div>
 <header id="branding" role="banner">
 <?php
 if (defined('WP_HOME') && WP_HOME != "http://cscircles.cemc.uwaterloo.ca/" && !get_option("cscircles_hide_ack", "0")=="1")
